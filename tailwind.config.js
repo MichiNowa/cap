b524c89ad8b401c306b2d7dcca0c565d0adc0268
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-08-16 15:09:54 +0000

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: 'tw-',
-  content: [
-    "./assets/public/js/**/*.{js,mjs}",
-    "./assets/public/vendor/**/*.{js,mjs,html}",
+  content: {
+    relative: true,
+    files: [
+    "./assets/public/js/**/*.{html,js,mjs}",
+    "./assets/public/vendor/**/*.{html,js,mjs,html}",
     "./assets/components/**/*.{php,html}",
     "./assets/layouts/**/*.{php,html}",
     "./assets/pages/**/*.{php,html}",
     "./assets/php/**/*.{php,html}",
-  ],
+    "./index.php",
+    ],
+  },
   theme: {
     extend: {},
   },
