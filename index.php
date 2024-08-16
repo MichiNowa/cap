3058789b67b3bdab35ea9fc46c8f07cb572a14ba
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-08-16 15:59:27 +0000

    with error handler

--- index.php
+++ index.php
@@ -12,21 +12,25 @@ $pageuri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $pagesearchparams = $_GET;
 
 
-//display pages from funtcion
-match ($pageuri) {
-    '/' => showPage('home', 'Home', [], 'auth'),
-    '/profile' => showPage('profile', 'Profile', ['user' => AUTHUSER], 'auth'),
-    '/assess' => showPage('assess', 'Self-Assessment Form', ['user' => AUTHUSER], 'auth'),
-    '/feedback' => showPage('feedback', 'Feedback Form', ['user' => AUTHUSER], 'auth'),
-    '/notif' => showPage('notif', 'Notification', ['user' => AUTHUSER], 'auth'),
-    '/signup' => showPage('signup', 'Sign Up'),
-    '/login' => showPage('login', 'Login'),
-    '/api/post/login' => showAPI('actions', 'POST'),
-    '/api/post/signup' => showAPI('actions', 'POST'),
-    '/api/post/logout' => showAPI('actions', 'POST'),
-    '/api/post/updateprofile' => showAPI('actions', 'POST'),
-    default => showPublicFolder('assets'),
-};
+//display pages from function
+try {
+    match ($pageuri) {
+        '/' => showPage('home', 'Home', [], 'auth'),
+        '/profile' => showPage('profile', 'Profile', ['user' => AUTHUSER], 'auth'),
+        '/assess' => showPage('assess', 'Self-Assessment Form', ['user' => AUTHUSER], 'auth'),
+        '/feedback' => showPage('feedback', 'Feedback Form', ['user' => AUTHUSER], 'auth'),
+        '/notif' => showPage('notif', 'Notification', ['user' => AUTHUSER], 'auth'),
+        '/signup' => showPage('signup', 'Sign Up'),
+        '/login' => showPage('login', 'Login'),
+        '/api/post/login' => showAPI('actions', 'POST'),
+        '/api/post/signup' => showAPI('actions', 'POST'),
+        '/api/post/logout' => showAPI('actions', 'POST'),
+        '/api/post/updateprofile' => showAPI('actions', 'POST'),
+        default => showPublicFolder('assets'),
+    };
+} catch (Throwable $e) {
+    showPage('error', 'Internal Server Error', ['error' => $e->getMessage()]);
+}
 
 unset($_SESSION['error']);
 unset($_SESSION['formdata']);
