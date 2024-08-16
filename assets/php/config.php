c76f086e4501faf34bd8d97df82e6dbb585ab626
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-08-16 14:41:48 +0000

    using define instead of const

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-const APP_TITLE = $_ENV['APP_TITLE'] ?? "SMCC";
-const URI_PREFIX = $_ENV['URI_PREFIX'] ?? "/" . basename(__DIR__);
+define('APP_TITLE', $_ENV['APP_TITLE'] ?? "SMCC");
+define('URI_PREFIX', $_ENV['URI_PREFIX'] ?? "/" . basename(__DIR__));
 
 // connect to dataabse 
-const DB_NAME = $_ENV['DB_NAME'] ?? 'gcms';
-const DB_HOST = $_ENV['DB_HOST'] ?? 'localhost';
-const DB_USER = $_ENV['DB_USER'] ?? 'root';
-const DB_PASS = $_ENV['DB_PASS'] ?? '';
+define('DB_NAME', $_ENV['DB_NAME'] ?? 'gcms');
+define('DB_HOST', $_ENV['DB_HOST'] ?? 'localhost');
+define('DB_USER', $_ENV['DB_USER'] ?? 'root');
+define('DB_PASS', $_ENV['DB_PASS'] ?? '');
