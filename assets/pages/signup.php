6fd3725a141f637ea644196994bd0fbb29caec01
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2024-08-18 02:57:29 +0000

    password toggle

--- assets/pages/signup.php
+++ assets/pages/signup.php
@@ -1,8 +1,8 @@
 <!-- regiuster form -->
 <div class="container fluid text-center justify-content-center">
-    <div class="row">
-        <div class="col-12 mt-3">
-            <img class="mb-4" src="<?= pathname('images/logo.jpg') ?>" alt="" height="200vh">
+    <div class="tw-flex tw-justify-center tw-items-start tw-mt-8">
+        <div class="tw-object-contain tw-p-4 tw-max-w-64 aspect-square">
+            <img class="mb-4" src="<?= pathname('images/logo.jpg') ?>" alt="Guidance Logo">
         </div>
     </div>
     <div class="row justify-content-center">
@@ -20,20 +20,20 @@
                 ?>
                 <div class="form-floating mt-1">
                     <input type="text" name="studentid" value="<?= showFormData('studentid') ?>"
-                        class="form-control rounded-5 " placeholder="">
+                        class="form-control rounded-3 " placeholder="">
                     <label for="floatingInput">Student or Teacher ID</label>
                 </div>
                 <?= showError('studentid') ?>
 
                 <div class="d-flex mt-1">
-                    <div class="form-floating mt-1 col-6 ">
+                    <div class="form-floating mt-1 col-6 tw-pr-1">
                         <input type="text" name="first_name" value="<?= showFormData('first_name') ?>"
-                            class="form-control rounded-5 " placeholder="">
+                            class="form-control rounded-3 " placeholder="">
                         <label for="floatingInput">First Name</label>
                     </div>
-                    <div class="form-floating mt-1 col-6">
+                    <div class="form-floating mt-1 col-6 tw-pl-1">
                         <input type="text" name="last_name" value="<?= showFormData('last_name') ?>"
-                            class="form-control rounded-5 " placeholder="">
+                            class="form-control rounded-3 " placeholder="">
                         <label for="floatingInput">Last Name</label>
                     </div>
                 </div>
@@ -59,17 +59,13 @@
                 </div> -->
                 <div class="form-floating mt-2">
                     <input type="email" name="email" value="<?= showFormData('email') ?>"
-                        class="form-control rounded-5 " placeholder="">
+                        class="form-control rounded-3 " placeholder="">
                     <label for="floatingInput">G-Suite Email</label>
                 </div>
                 <?= showError('email') ?>
-
-                <div class="text-end mt-2">
-                    <input type="checkbox" name="pswd_show" id="pswd_show" onclick="pswd_toggle()"><small> Show
-                        Password</small>
-                </div>
-                <div class="form-floating mt-1">
-                    <input type="password" name="password" class="form-control rounded-5 " id="floatingPassword"
+                <div class="form-floating mt-1 position-relative">
+                    <button type="button" id="pswd_show" class="position-absolute tw-right-0 tw-top-0 tw-h-full tw-aspect-square p-2 z-100" onclick="pswd_toggle()" data-show="false"><i class="bi bi-eye-slash-fill"></i></button>
+                    <input type="password" name="password" class="form-control rounded-3 " id="floatingPassword"
                         placeholder="Password">
                     <label for="floatingPassword">Password</label>
                 </div>
@@ -92,10 +88,17 @@
     function pswd_toggle() {
         var x = document.getElementById("floatingPassword");
         var box = document.getElementById("pswd_show");
-        if (box.checked == 1) {
+
+        if (box.dataset.show == "false") {
             x.type = "text";
+            box.firstElementChild.classList.remove("bi-eye-slash-fill");
+            box.firstElementChild.classList.add("bi-eye-fill");
+            box.setAttribute("data-show", "true");
         } else {
             x.type = "password";
+            box.firstElementChild.classList.remove("bi-eye-fill");
+            box.firstElementChild.classList.add("bi-eye-slash-fill");
+            box.setAttribute("data-show", "false");
         }
     }
 </script>
